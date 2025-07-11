13da7afe5e4f8b5283da6e34005b4b91c08a7f02
Ilhomjon <futurewithme1221@gmail.com>
2025-07-11 22:54:52 +0500

    feat: Added phone column to user table

--- backend/models/user.go
+++ backend/models/user.go
@@ -6,5 +6,6 @@ type User struct {
 	Address     string      `json:"address"`
 	Pre_payment uint        `json:"pre_payment"`
 	Active      bool        `json:"active"`
+	Phones      []string    `gorm:"serializer:json" json:"phones"`
 	RentTools   []RentTools `json:"rent_tools"`
 }
