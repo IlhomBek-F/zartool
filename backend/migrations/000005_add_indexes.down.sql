b3e732e3825858656c9a87dd2a3f6183405027d7
Ilhomjon <futurewithme1221@gmail.com>
2025-08-04 10:04:38 +0500

    feat: [BACK] Implemented indexes to users fullname and phones column

---
+++ backend/migrations/000005_add_indexes.down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS idx_users_full_name ON users;
+DROP INDEX IF EXISTS idx_users_phones ON users;
---
+++ backend/migrations/000005_add_indexes.up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_users_full_name ON users (full_name); 
+CREATE INDEX IF NOT EXISTS idx_users_phones on users (phones);    
